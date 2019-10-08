6047d75dfa7a29551581a03db79c991aa96d43f4
Charline SEBELLIN <20190177@B15-06.local>
2019-10-08 11:50:20 +0200

    upperCase and lowerCase

---
+++ ex01.js
@@ -0,0 +1,3 @@
+module.exports = function lowercase(str = '') {
+    return str.toLowerCase()
+}
---
+++ main.js
@@ -0,0 +1,10 @@
+const lowerCase = require('./ex01')
+const upperCase = require('./ex02')
+
+console.log('lowerCase:')
+console.log(lowerCase('Chopper'))
+console.log(lowerCase('TONY TONY'))
+
+console.log('upperCase:')
+console.log(upperCase('Choper'))
+console.log(upperCase('TONY TONY'))
